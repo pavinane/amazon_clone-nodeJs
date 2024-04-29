52524d6f62029209d0f2668ee251140798771fac
Pavithranath <pavimegan77@gmail.com>
2024-04-29 09:51:38 +0530

    create like and dislike api

--- server/controller/blogController.js
+++ server/controller/blogController.js
@@ -12,7 +12,7 @@ const createBlog = asyncHandler(async (req, res) => {
 });
 const updatBlog = asyncHandler(async (req, res) => {
   const { id } = req.params;
-  //   validateMongoDBId(id);
+  validateMongoDBId(id);
   try {
     const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, {
       new: true,
@@ -112,55 +112,53 @@ const likeBlog = asyncHandler(async (req, res) => {
   }
 });
 
-// const likeBlog = asyncHandler(async (req, res) => {
-//   const { blogId } = req.body;
+const disLikeBlog = asyncHandler(async (req, res) => {
+  const { blogId } = req.body;
 
-//   // Check if blogId is a valid ObjectId
-//   if (!mongoose.Types.ObjectId.isValid(blogId)) {
-//     console.log("Invalid blogId:", blogId);
-//     return res.status(400).json({ message: "Invalid blogId" });
-//   }
+  validateMongoDBId(blogId);
+  //   find the blog which one you want to be like
+  const blog = await Blog.findById(blogId);
+  //  Find the user id
+  const loginUserId = req?.user?._id;
+  // find if the user liked the blog
+  const isDisLiked = blog?.isDisliked;
 
-//   try {
-//     // Find the blog by its Id
-//     const blog = await Blog.findById(blogId);
-//     if (!blog) {
-//       console.log("Blog not found for blogId:", blogId);
-//       return res.status(404).json({ message: "Blog not found" });
-//     }
-
-//     // Find the user's ID
-//     const loginUserId = req.user?._id;
-//     if (!loginUserId) {
-//       console.log("User not authenticated");
-//       return res.status(401).json({ message: "User not authenticated" });
-//     }
-
-//     // Check if the user has already liked the blog
-//     const alreadyLiked = blog.likes.includes(loginUserId);
-
-//     if (alreadyLiked) {
-//       // If already liked, remove the like
-//       await Blog.findByIdAndUpdate(blogId, {
-//         $pull: { likes: loginUserId },
-//         isLiked: false,
-//       });
-//     } else {
-//       // If not liked, add the like
-//       await Blog.findByIdAndUpdate(blogId, {
-//         $addToSet: { likes: loginUserId },
-//         isLiked: true,
-//       });
-//     }
-
-//     // Return the updated blog
-//     const updatedBlog = await Blog.findById(blogId);
-//     res.json(updatedBlog);
-//   } catch (error) {
-//     console.error("Error in likeBlog:", error);
-//     res.status(500).json({ message: "Internal server error" });
-//   }
-// });
+  const alreadyLiked = blog?.likes?.find(
+    (userId) => userId?.toString() === loginUserId?.toString()
+  );
+  if (alreadyLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { likes: loginUserId },
+        isLiked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+  if (isDisLiked) {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $pull: { dislikes: loginUserId },
+        isDisliked: false,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  } else {
+    const blog = await Blog.findByIdAndUpdate(
+      blogId,
+      {
+        $push: { dislikes: loginUserId },
+        isDisliked: true,
+      },
+      { new: true }
+    );
+    res.json(blog);
+  }
+});
 
 module.exports = {
   createBlog,
@@ -169,4 +167,5 @@ module.exports = {
   getAllBlogs,
   deleteBlogs,
   likeBlog,
+  disLikeBlog,
 };
--- server/routes/blogRoutes.js
+++ server/routes/blogRoutes.js
@@ -7,12 +7,15 @@ const {
   getAllBlogs,
   deleteBlogs,
   likeBlog,
+  disLikeBlog,
 } = require("../controller/blogController");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createBlog);
 router.put("/likes", authMiddleware, likeBlog);
+router.put("/dislikes", authMiddleware, disLikeBlog);
 router.put("/:id", authMiddleware, isAdmin, updatBlog);
+
 router.get("/:id", idBlog);
 router.get("/", getAllBlogs);
 router.delete("/:id", authMiddleware, isAdmin, deleteBlogs);
