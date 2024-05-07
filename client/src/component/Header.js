4bb1cc9d118f1b3079632daf45fdd462253fad66
Pavithranath <pavimegan77@gmail.com>
2024-05-08 01:10:20 +0530

    create header

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,7 +1,57 @@
 import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline :&nbsp;
+                <a href="tel:+91 8072329880" className="text-white">
+                  +91 8072329880
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Buy Corner</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group ">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
