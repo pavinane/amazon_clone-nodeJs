55a4c109ebed43b3e2a777b8792425a79cc2cb6a
Pavithranath <pavimegan77@gmail.com>
2024-05-24 18:31:28 +0530

    add product and blog cards

---
+++ client/src/component/ProductCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+
+function ProductCard() {
+  return (
+    <div className="col-3">
+      <div className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="" />
+          </Link>
+        </div>
+        <div className="product-image">
+          <img src="images/watch.jpg" alt="product images" />
+        </div>
+        <div className="product-details">
+          <h6 className="brand"> Havels</h6>
+          <h5 className="brand">
+            {" "}
+            Smart Watch bulk 10 multi color for students
+          </h5>
+          <ReactStars
+            count={5}
+            // onChange={ratingChanged}
+            value={3}
+            edit={false}
+            size={15}
+            activeColor="#ffd700"
+          />
+          <p className="price">$100.00</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/prodcompare.svg" alt="compare" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/add-cart.svg" alt="cart" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
 import BlogCard from "../component/BlogCard";
+import ProductCard from "../component/ProductCard";
 
 function Home() {
   return (
@@ -235,6 +236,21 @@ function Home() {
           </div>
         </div>
       </section>
+
+      <section className="blog-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h3 className="section-heading">Feature Collection</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </section>
+
       <section className="blog-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
